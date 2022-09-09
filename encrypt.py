edcafda47c8af027f6913ef71ff7769aa7da0f73
YeetingWaterbottle <73762047+YeetingWaterbottle@users.noreply.github.com>
2022-09-08 23:53:40 -0700

    organize code into functions

--- encrypt.py
+++ encrypt.py
@@ -1,26 +1,55 @@
 from math import sqrt, floor, ceil
 
+def get_list_width(byte_list_length):
+    if floor(sqrt(byte_list_length)) % 2 == 0:
+        return floor(sqrt(byte_list_length))
+ 
+    elif ceil(sqrt(byte_list_length)) % 2 == 0:
+        return ceil(sqrt(byte_list_length))
+
+def str_to_byte(input_str):
+    result = []
+    for i in bytearray(input_str, encoding='utf-8'):
+        result.append(format(i, '08b'))
+
+    return result
+
+def set_array_width(byte_list, byte_list_length, list_width):
+    result = []
+    for i in range(0, byte_list_length, list_width):
+        result.append(byte_list[i:i + list_width])
+
+    return result
+    
+
+def check_add_padding(byte_list, list_width):
+    if len(byte_list[-1]) != list_width:
+        byte_list[-1] += (list_width - len(byte_list[-1])) * ["00000000"]
+
+    return byte_list
+
+def split_bytes(byte_list):
+    result = []
+    for row in byte_list:
+        for byte in row:
+            half = len(byte)//2
+            result.append([byte[:half], byte[half:]])
+
+    return result
+
 input_str = "take the total of the binaries and then square root it and minus one."
 input_len = len(input_str)
-input_bin = []
+input_bin = str_to_byte(input_str)
 result = []
 
-for i in bytearray(input_str, encoding='utf-8'):
-    input_bin.append(format(i, '08b'))
+width_segment = get_list_width(input_len)
 
-width_segment = floor(sqrt(input_len))
+result = set_array_width(input_bin, input_len, width_segment)
 
-if floor(sqrt(input_len)) % 2 == 0:
-    width_segment = floor(sqrt(input_len))
+result = check_add_padding(result, width_segment)
 
-elif ceil(sqrt(input_len)) % 2 == 0:
-    width_segment = ceil(sqrt(input_len))
+result = split_bytes(result)
 
-for i in range(0, input_len, width_segment):
-    result.append(input_bin[i:i + width_segment])
-
-if len(result[-1]) != width_segment:
-    result[-1] += (width_segment - len(result[-1])) * ["00000000"]
 
 for i in result:
     print(i)
