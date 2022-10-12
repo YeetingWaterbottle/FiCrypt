6cf4958a4c5e25c6e0c61ed7a8e0759e9f7e636d
YeetingWaterbottle <73762047+YeetingWaterbottle@users.noreply.github.com>
2022-10-11 22:58:52 -0700

    optmize many functions with numpy and new methods

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import io
 from flask import Flask, request, render_template, send_file
 from math import sqrt, floor, ceil
 import numpy as np
-import base64
+from base64 import b64encode, b64decode
+import itertools
 
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
@@ -14,25 +15,32 @@ def get_list_width(byte_list_length, divide):
     if floor(radicand) % 2 == 0:
         return floor(radicand)
 
-    elif ceil(radicand) % 2 == 0:
+    else:
         return ceil(radicand)
 
-    return -1
+def convert_base64(string, action):
+    if action == "en":
+        return b64encode(string.encode()).decode()
+
+    elif action == "de":
+        return b64decode(string).decode()
 
 
 def str_to_byte(input, type, file_name=""):
     result = []
     if type == "string":
         for i in bytearray(input, encoding="utf8"):
-            result.append(format(i, '08b'))
+            result.append(format(i, "08b"))
 
     elif type == "binary":
         for i in bytearray(input):
-            result.append(format(i, '08b'))
+            result.append(format(i, "08b"))
 
     if file_name != "":
         result.extend(["00000000"] * 8)
-        result.extend(str_to_byte(file_name, 'string'))
+        result.extend(str_to_byte(convert_base64(file_name, "en"), "string"))
+        result.extend(["00000000"] * 4)
+        result.extend(str_to_byte(str(len(file_name)), "string"))
 
     return result
 
@@ -52,114 +60,97 @@ def check_add_padding(byte_list, list_width):
     return byte_list
 
 
-def split_bytes(byte_list):
-    result = []
-    for row in byte_list:
-        foo = ""
-        for byte in row:
-            half = len(byte) // 2
-            foo += f"{byte[:half]} {byte[half:]} "
-        result.append(foo.strip().split(" "))
+def split_bytes(byte_list, list_width):
+    result = "".join(list(itertools.chain.from_iterable(byte_list)))
 
-    return result
+    result = [result[i:i+4] for i in range(0, len(result), 4)]
+
+    return set_array_width(result, len(result), list_width)
 
 
-def combine_bytes(byte_list, count):
-    result = []
-    for row in byte_list:
-        foo = ""
-        for byte in range(0, len(row), count):
-            foo += "".join(row[byte:byte + count]) + " "
-        result.append(foo.strip().split(" "))
+def combine_bytes(byte_list, list_width):
+    result = "".join(list(itertools.chain.from_iterable(byte_list)))
 
-    return result
+    result = [result[i:i+8] for i in range(0, len(result), 8)]
+
+    return set_array_width(result, len(result), list_width)
 
 
 def reverse_bytes(byte_list, direction, index=0):
     if direction == "horizontal":
-        result = []
         if index == 0:
-            for row in byte_list:
-                result.append(row[::-1])
-            return result
+            return np.fliplr(byte_list)
+            
+        return np.concatenate((byte_list[:, :index], np.fliplr(byte_list[:, index:])), axis=1)
 
-        for row in byte_list:
-            result.append(row[:index] + row[:index - 1:-1])
-        return result
 
     elif direction == "verticle":
         if index == 0:
-            return byte_list[::-1]
+            return np.flipud(byte_list)
 
-        return byte_list[:index] + byte_list[:index - 1:-1]
+        return np.concatenate((byte_list[:index], np.flipud(byte_list[index:])), axis=0)
 
-    return -1
-
-
-def mirror_bytes(byte_list):
-    result = []
-    for row in byte_list:
-        foo = " ".join(row)
-
-        foo = foo.translate(str.maketrans('01', '10'))
-
-        result.append(foo.split(" "))
-
-    return result
+    raise Exception("direction inputted does not match any presets.")
 
 
-def encrypt_bytes(byte_list, password):
+def mirror_bytes(byte_list, width_segment):
+    result = "".join(list(itertools.chain.from_iterable(byte_list)))
+
+    result = result.translate(str.maketrans('01', '10'))
+
+    result = [result[i:i+4] for i in range(0, len(result), 4)]
+
+    return set_array_width(result, len(result), width_segment)
+
+
+def encrypt_bytes(byte_list, password, width_segment):
     password = str_to_byte(password, "string")
-    password = split_bytes([password])[0]
+    password = split_bytes([password], len(password) * 2)[0]
 
     for byte in password:
         if byte[0] == "1":
-            byte_list = mirror_bytes(byte_list)
-            # print("Mirror Bytes")
+            print("Mirror Bytes")
+            byte_list = mirror_bytes(byte_list, width_segment)
         if byte[1] == "1":
+            print("Reverse Bytes Horizontally")
             byte_list = reverse_bytes(byte_list, "horizontal")
-            # print("Reverse Bytes Horizontally")
         if byte[2] == "1":
+            print("Reverse Bytes Vertically")
             byte_list = reverse_bytes(byte_list, "verticle")
-            # print("Reverse Bytes Vertically")
         if byte[3] == "1":
             if byte[1] == "1":
-                byte_list = reverse_bytes(byte_list, "horizontal",
-                                          len(byte_list[0]) // 2)
-                # print("Reverse Bytes Half Horizontally")
+                print("Reverse Bytes Half Horizontally")
+                byte_list = reverse_bytes(byte_list, "horizontal", len(byte_list[0]) // 2)
             if byte[2] == "1":
-                byte_list = reverse_bytes(byte_list, "verticle",
-                                          len(byte_list) // 2)
-                # print("Reverse Bytes Half Vertically")
+                print("Reverse Bytes Half Vertically")
+                byte_list = reverse_bytes(byte_list, "verticle", len(byte_list) // 2)
     return byte_list
 
 
-def decrypt_bytes(byte_list, password):
+def decrypt_bytes(byte_list, password, width_segment):
     password = str_to_byte(password, "string")
     password = split_bytes([password])[0]
 
     for byte in password[::-1]:
         if byte[3] == "1":
             if byte[2] == "1":
-                byte_list = reverse_bytes(byte_list, "verticle",
-                                          len(byte_list) // 2)
-                # print("Reverse Bytes Half Vertically")
+                print("Reverse Bytes Half Vertically")
+                byte_list = reverse_bytes(byte_list, "verticle", len(byte_list) // 2)
             if byte[1] == "1":
-                byte_list = reverse_bytes(byte_list, "horizontal",
-                                          len(byte_list[0]) // 2)
-                # print("Reverse Bytes Half Horizontally")
+                print("Reverse Bytes Half Horizontally")
+                byte_list = reverse_bytes(byte_list, "horizontal", len(byte_list[0]) // 2)
 
         if byte[2] == "1":
+            print("Reverse Bytes Vertically")
             byte_list = reverse_bytes(byte_list, "verticle")
-            # print("Reverse Bytes Vertically")
 
         if byte[1] == "1":
+            print("Reverse Bytes Horizontally")
             byte_list = reverse_bytes(byte_list, "horizontal")
-            # print("Reverse Bytes Horizontally")
 
         if byte[0] == "1":
-            byte_list = mirror_bytes(byte_list)
-            # print("Mirror Bytes")
+            print("Mirror Bytes")
+            byte_list = mirror_bytes(byte_list, width_segment)
 
     return byte_list
 
@@ -213,32 +204,38 @@ def file_encryption(action, file_input, password):
 
     input_str = file_input.read()
 
+    print("to byte")
     if action == "en":
         input_bin = str_to_byte(input_str, "binary", file_name)
 
     if action == "de":
         input_bin = str_to_byte(input_str, "binary")
+    print("dont to byte")
 
     input_len = len(input_bin)
     result = []
 
     width_segment = get_list_width(input_len, 2)
-
+    print("to 2d")
     result = set_array_width(input_bin, input_len, width_segment)
+    print("done to 2d")
 
+    print("add padd1")
     result = check_add_padding(result, width_segment)
+    print("done pad")
 
-    result = split_bytes(result)
-
+    print("split in half")
+    result = split_bytes(result, width_segment)
+    print("done split in half")
 
     if action == "en":
-        result = encrypt_bytes(result, password)
+        result = encrypt_bytes(result, password, width_segment)
 
     if action == "de":
-        result = decrypt_bytes(result, password)
+        result = decrypt_bytes(result, password, width_segment)
 
     # print("Combined Back to 8-bit Bytes")
-    result = combine_bytes(result, 2)
+    result = combine_bytes(result, width_segment)
 
     if action == "en":
         return save_bytes(result, action, "encrypted.enc")
